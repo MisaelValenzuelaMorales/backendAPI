893800e73f0eaec724a56fb7aaec6cf783fb68dd
MisaelValenzuelaMorales <misaelvalenzuelamorales@gmail.com>
2024-12-06 21:31:55 -0600

    Conexión a la BD

--- test/src/app.module.ts
+++ test/src/app.module.ts
@@ -1,7 +1,23 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    
+    ConfigModule.forRoot({
+      isGlobal: true, // variables de entorno disponibles globalmente en el proyecto
+    }),
+
+    // Configurar la conexión con MongoDB usando Mongoose
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule], // Importar ConfigModule para acceder a las variables de entorno
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'), // Acceder a la URI de MongoDB
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
